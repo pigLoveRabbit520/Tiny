43f89e84ee0496a1bc9a5935cd1fb2d9d3145e67
salamander-mh <1906747819@qq.com>
2018-05-25 13:45:19 +0800

    fix bug comment的Author字段为string

--- models/comment.go
+++ models/comment.go
@@ -10,7 +10,7 @@ type Comment struct {
 	Coid     uint   `gorm:"primary_key" json:"coid"`
 	Cid      uint   `gorm:"type:int(10) unsigned" json:"cid"`
 	Created  uint   `gorm:"type:int(10) unsigned" json:"created"`
-	Author   uint   `gorm:"type:varchar(200)" json:"author"`
+	Author   string `gorm:"type:varchar(200)" json:"author"`
 	AuthorId uint   `gorm:"column:authorId;type:int(10) unsigned" json:"authorId"`
 	Mail     string `gorm:"type:varchar(200)" json:"mail"`
 	URL      string `gorm:"type:varchar(200)" json:"url"`
@@ -33,19 +33,13 @@ func GetComments(page int) ([]CommentWithContent, int, error) {
 	prefix := config.Conf.DB.Prefix
 	tbContent := prefix + "contents"
 	tbComment := prefix + "comments"
-	rows, err := db.DB.Table(fmt.Sprintf("%s com", tbComment)).Select(`con.cid, com.created, author, mail, url, com.text, ip, com.status, 
+	q := db.DB.Table(fmt.Sprintf("%s com", tbComment)).Select(`con.cid, com.created, author, mail, url, com.text, ip, com.status, 
 			com.parent, con.title`).
 		Joins("LEFT JOIN "+tbContent+" con ON con.cid = com.cid").
 		Where("com.type = ?", "comment").Order("com.created DESC").Limit(PAGE_SIZE).Offset((page - 1) * PAGE_SIZE).
-		Rows()
-	if err != nil {
-		return nil, -1, err
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var com CommentWithContent
-		db.DB.ScanRows(rows, &com)
-		comments = append(comments, com)
+		Find(&comments)
+	if q.Error != nil {
+		return nil, -1, q.Error
 	}
 	db.DB.Table(tbComment).Count(&total)
 	return comments, total, nil
